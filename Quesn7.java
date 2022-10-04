c9879a5d7342316f80d98b07e157a923ce313843
ShipraRaj <114066544+ShipraRaj@users.noreply.github.com>
2022-10-04 11:45:57 +0530

    Add files via upload

---
+++ Quesn7.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.Map;
+import java.uitl.Set;
+public class ListSetMap{
+public static void main(String[] args){
+System.out.println(List.of(1,2,3,4,5,6,7,8));
+System.out.println(Set.of(1,2,3,4,5,6,7,8));
+System.out.println(Map.of(1,"one",2,"two",3,"three",4,"four"));
+}
+}
